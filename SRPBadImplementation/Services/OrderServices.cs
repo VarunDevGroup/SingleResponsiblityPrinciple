3693f5bdff13061e243bafd00f5548ba2e2b533c
Varun <Varun@VARUNPC>
2026-02-17 19:58:30 +0530

    Add project files.

---
+++ SRPBadImplementation/Services/OrderServices.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SRPBadImplementation.Services
+{
+    internal class OrderServices
+    {
+        public void CreateOrder()
+        {
+            //Code to create order entity
+            Console.WriteLine("Creatign Order");
+
+            SaveData();
+
+            SendMail();
+        }
+
+        public void SaveData()
+        {
+            //Code to save data to database
+            Console.WriteLine("Saving data to database");
+        }
+
+        public void SendMail()
+        {
+            //Code to send email
+            Console.WriteLine("Sending email");
+        }
+    }
+}
